faf74bd2ab984522b425ee0a070a4982bd8c0e6a
Ekin Ilter Varli <ekinnos@tutanota.com>
2024-02-25 18:24:42 +0300

    .

--- social/settings.py
+++ social/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'compressor',
     'network',
     'posts',
     'account',
@@ -133,13 +132,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/') 
-
-STATICFILES_FINDERS =( 
-    'django.contrib.staticfiles.finders.FileSystemFinder',  'django.contrib.staticfiles.finders.AppDirectoriesFinder',    'compressor.finders.CompressorFinder',
-)
-COMPRESS_PRECOMPILERS = (    
-    ('text/x-scss', 'django_libsass.SassCompiler'),
-)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
