2a25e09ec528f20371db1c0a31b68a3d24eb80a7
Ekin Ilter Varli <ekinnos@tutanota.com>
2024-03-28 23:57:24 +0300

    Updated scripts UI

--- platform/scripts/admin.py
+++ platform/scripts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from scripts.models import Script, ScriptCategory
 
 # Register your models here.
+admin.site.register(Script)
+admin.site.register(ScriptCategory)
--- platform/scripts/views/index.py
+++ platform/scripts/views/index.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from scripts.models import Script
 
 
 @login_required
 def index(request):
-    return render(request, 'scripts/index.jinja', context={})
+    scripts = Script.objects.all()
+    return render(request, 'scripts/index.jinja', context={
+        'scripts': scripts,
+    })
