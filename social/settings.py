18f4d6f0ef19ec712369fb16a64fe784f2f17929
Ekin Varlı <ekinnos@tutanota.com>
2025-03-22 05:00:19 +0300

    .

--- social/settings.py
+++ social/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','retto.social','www.retto.social']
 #CSRF_TRUSTED_ORIGINS = ['domain.com','http://127.0.0.1:8000']
 
 CSRF_COOKIE_SECURE = False
@@ -131,10 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_URL = 'assets/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'assets'
 ] 
 
 MEDIA_URL = '/media/'
