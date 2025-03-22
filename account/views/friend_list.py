7b82d681c8201e925f570ada4e43a57df8698652
Ekin Varlı <ekinnos@tutanota.com>
2025-03-22 19:31:09 +0300

    .

--- account/views/friend_list.py
+++ account/views/friend_list.py
@@ -3,7 +3,6 @@ from account.models import Friend
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 
-
 @login_required
 def friend_list(request):
     # Kabul edilen arkadaşlar (İki yönlü kontrol)
@@ -16,10 +15,16 @@ def friend_list(request):
         to_user=request.user, is_accepted=False
     )
 
-    # Arama fonksiyonu
+    # Arama fonksiyonu (friend_list içinde arama yapıyoruz)
     search = request.GET.get('search')
     if search:
         friend_list = friend_list.filter(
             Q(from_user__username__icontains=search) |
             Q(to_user__username__icontains=search)
-        ).distinct
+        ).distinct()
+
+    # **Hata önleme:** Eğer `friend_list` ve `friend_requests` boşsa bile `return` olmalı!
+    return render(request, 'account/friend-list.jinja', {
+        'friend_list': friend_list if friend_list.exists() else [],
+        'friend_requests': friend_requests if friend_requests.exists() else []
+    })
