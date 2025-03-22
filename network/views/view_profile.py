865bf57e9092281cd589e52acae9d0f431fb0b25
Ekin Varlı <ekinnos@tutanota.com>
2025-03-22 05:10:51 +0300

    .

--- network/views/view_profile.py
+++ network/views/view_profile.py
@@ -1,31 +1,41 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
 from network.models import CustomUser
 from posts.models import Post
 from account.models import Friend
 from django.db.models import Q
 
-
-
 def view_profile(request, username):
-    user = get_object_or_404(CustomUser, username=username)
+    # Kullanıcının siteye kayıtlı olup olmadığını kontrol et
+    try:
+        user = CustomUser.objects.get(username=username)  # Bu satırda hata alırsanız, kullanıcı yok demektir.
+    except CustomUser.DoesNotExist:
+        return redirect('index')  # Eğer kullanıcı yoksa, anasayfaya yönlendir.
+
+    # Kullanıcıya ait gönderileri al
     posts = Post.objects.filter(author=user)
+
     if request.user.is_authenticated:
-        users = CustomUser.objects.all()
-        if user in users:
-            if user != request.user:
-                friend_request_sent = Friend.objects.filter(from_user=request.user, to_user=user, is_accepted=False).exists()
-                is_friend = Friend.objects.filter((Q(from_user=request.user, to_user=user) | Q(from_user=user, to_user=request.user)), is_accepted=True).exists()
+        # Kullanıcı kendi profilini görüntülemeye çalışıyorsa
+        if user != request.user:
+            # Arkadaşlık isteği durumu kontrolü
+            friend_request_sent = Friend.objects.filter(from_user=request.user, to_user=user, is_accepted=False).exists()
 
+            # Arkadaşlık durumu kontrolü
+            is_friend = Friend.objects.filter(
+                (Q(from_user=request.user, to_user=user) | Q(from_user=user, to_user=request.user)),
+                is_accepted=True
+            ).exists()
 
-                return render(request, 'network/user-profile.jinja', context={
-                    'user':user,
-                    'posts':posts,
-                    'friend_request_sent': friend_request_sent,
-                    'is_friend': is_friend,
-                })
-            else:
-                return redirect('profile')
+            # Profil sayfasını render et
+            return render(request, 'network/user-profile.jinja', context={
+                'user': user,
+                'posts': posts,
+                'friend_request_sent': friend_request_sent,
+                'is_friend': is_friend,
+            })
         else:
-            return redirect('index')
+            # Eğer kullanıcı kendi profilini görüntülüyorsa, profil sayfasına yönlendir
+            return redirect('profile')
     else:
+        # Giriş yapmamış kullanıcıyı anasayfaya yönlendir
         return redirect('index')
