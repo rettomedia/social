ca3e62e651dd0b3e9b38143adfc22efdc010ed55
Ekin Varlı <ekinnos@tutanota.com>
2025-03-22 19:25:10 +0300

    .

--- account/views/friend_list.py
+++ account/views/friend_list.py
@@ -6,24 +6,20 @@ from django.db.models import Q
 
 @login_required
 def friend_list(request):
+    # Kabul edilen arkadaşlar (İki yönlü kontrol)
     friend_list = Friend.objects.filter(
-        Q(from_user=request.user) |
-        Q(to_user=request.user), is_accepted=True
+        Q(from_user=request.user) | Q(to_user=request.user), is_accepted=True
     )
 
+    # Yalnızca size gelen istekleri al (Sizin gönderdiğiniz istekleri filtreleme!)
     friend_requests = Friend.objects.filter(
-        Q(from_user=request.user) |
-        Q(to_user=request.user), is_accepted=False
+        to_user=request.user, is_accepted=False
     )
 
+    # Arama fonksiyonu
     search = request.GET.get('search')
     if search:
         friend_list = friend_list.filter(
-            Q(from_user__username__contains=search) |
-            Q(to_user__username__contains=search)
-        ).distinct()
-
-    return render(request, 'account/friend-list.jinja', context={
-        'friend_list':friend_list,
-        'friend_requests':friend_requests,
-    })
+            Q(from_user__username__icontains=search) |
+            Q(to_user__username__icontains=search)
+        ).distinct
