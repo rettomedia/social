aeb142cf73e7670425a74d45f6d51f9e9ea386dc
Ekin Varlı <ekinnos@tutanota.com>
2025-03-23 17:13:09 +0300

    Updated ticket backend

--- network/models/tickets.py
+++ network/models/tickets.py
@@ -3,8 +3,9 @@ from network.models.user import CustomUser
 
 
 class Ticket(models.Model):
-    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, blank=True, null=True)
     content = models.TextField(max_length=500, verbose_name='ticket')
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = 'ticket'
